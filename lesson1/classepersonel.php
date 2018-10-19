1b91b3fed3fd09e4c9d0f377c0c14e9bd9bdcd81
elvis kankola <elviskankola1@gmail>
2018-10-19 15:21:59 +0200

    UPDATE

--- lesson1/classepersonel.php
+++ lesson1/classepersonel.php
@@ -4,11 +4,15 @@
     private $nom_auditoire;
     private $professeur_auditoire;
     private $nb_eleve_auditoire;
-
+//========================================CONSTRUCTEUR=============================================
     function __construct(string $classe, string $prof, int $nb){
 
-    }
+        $this->set_nom_auditoire($classe);
+        $this->set_professeur_auditoire($prof);
+        $this->set_nb_eleve_auditoire($nb);
 
+    }
+//===================================SETTER'S======================================================
     public function set_nom_auditoire( string $nom){
         $this->nom_auditoire = $nom;
     }
@@ -17,6 +21,16 @@
     }
     public function set_nb_eleve_auditoire(int $nb){
         $this->nb_eleve_auditoire = $nb;
+    }
+//================================GETTER'S========================================================
+    public function get_nom_auditoire(){
+        return $this->nom_auditoire;
+    }
+    public function get_professeur_auditoire(){
+        return $this->professeur_auditoire;
+    }
+    public function get_nb_eleve(){
+        return $this->nb_eleve_auditoire;
     }
 
 
