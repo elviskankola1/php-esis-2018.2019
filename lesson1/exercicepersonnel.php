f0b1f50c422381b3f2e7c0669adccb77bde25b1f
elvis kankola <elviskankola1@gmail>
2018-10-19 13:46:52 +0200

    TEST

--- lesson1/exercicepersonnel.php
+++ lesson1/exercicepersonnel.php
@@ -1,5 +1,5 @@
 <?php
-
+//============================================FUNCTION'S===========================================================
     function bonjour_genie_log(){
         return "bonjour denie logiciel";
     }
@@ -7,5 +7,11 @@
     function addition(int $a, int $b){
          $s = $a+$b;
          return $s;
+    }
+  // ==============================================ARRAY============================================================
+    $tab = array('elvis','kankola','tshibala','valentin','html','python','ville');
+    $taille = count($tab);
+    for ($i=0; $i < $taille; $i++) { 
+        print(tab[$i]);
     }
 ?>
