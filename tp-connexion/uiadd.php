baf3f9d58c6866f618eb72ba5f2ae56e0af714ff
elvis kankola <elviskankola1@gmail>
2018-11-05 09:32:12 +0200

    UPDATE

--- tp-connexion/uiadd.php
+++ tp-connexion/uiadd.php
@@ -5,7 +5,7 @@
     <title>AJOUT</title>
 </head>
 <html>
-    <form action="add.php" method="POST" >
+    <form action="add.php" method="post" >
         <input type='text' placeholder="entrez votre pseudo" name="pseudo"><br><br>
         <input type="email" placeholder="entrez votre adresse e-mail" name="mail"><br><br>
         <input type="password" placeholder="entrez votre mot de passe" name="pwd"><br><br>
