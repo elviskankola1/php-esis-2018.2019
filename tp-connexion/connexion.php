9db30ef5fb3c9760d867c4ea56dbed2ac2d39400
elvis kankola <elviskankola1@gmail>
2018-11-05 10:14:17 +0200

    UPDATE

--- tp-connexion/connexion.php
+++ tp-connexion/connexion.php
@@ -1,1 +1,13 @@
+<?php
+     $db = new PDO('mysql:host=localhost;dbname=data-try','root','');
+     if ($_SERVER['REQUEST_METHOD'] =='POST') {
+        $pseudo = $_POST['pseudo'];
+        $pwd = $_POST['pwd'];
+        $user = $db->prepare('SELECT (pseudo,pwd) INTO user WHERE (pseudo =:pseudo AND pwd =: pwd)');
+        if ($user) {
+            echo "COMPTE VALIDE";
+        }else{
+            $_SERVER['HTTP_REFERER'];
+        }
 
+     }
