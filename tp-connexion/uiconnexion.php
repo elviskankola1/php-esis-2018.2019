a159c6d9330da5d176c79c586f72d6e1df3d5052
elvis kankola <elviskankola1@gmail>
2018-11-05 10:14:22 +0200

    UPDATE

--- tp-connexion/uiconnexion.php
+++ tp-connexion/uiconnexion.php
@@ -1,1 +1,15 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CONNEXION</title>
+</head>
+<html>
+    <form action="add.php" method="post" >
+        <input type='text' placeholder="entrez votre pseudo" name="pseudo"><br><br>
+        <input type="password" placeholder="entrez votre mot de passe" name="pwd"><br><br>
+        <input type="submit" value="CONNEXION">
+    </form>
+    
+</html>
+
