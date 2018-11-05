a10e7a9674732252d24fd888b4867463c53610eb
elvis kankola <elviskankola1@gmail>
2018-11-05 09:50:23 +0200

    UPDATE

--- tp-connexion/add.php
+++ tp-connexion/add.php
@@ -1,2 +1,24 @@
 <?php
-    echo 'hello';
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+        
+        $pseudo = strip_tags(trim($_POST['pseudo']));
+        $pwd = strip_tags(trim($_POST['pwd']));
+        $pwdc = strip_tags(trim($_POST['pwdc']));
+        $sexe = $_POST['sexe'];
+        $adressemail = strip_tags(trim($_POST['mail']));
+        if (empty($pseudo) OR empty($pwd) OR empty($pwdc) OR empty($sexe) OR empty($adressemail) OR !isset($pseudo,$pwd,$pwdc,$sexe,$adressemail)) {
+            
+            header('location:uiadd.php');
+        }else{
+            $db = new PDO('mysql:host=localhost;dbname=data-try','root','');
+            $query = $db->prepare('INSERT INTO user(pseudo,pwd,sexe,mail) VALUES (:pseudo,:pwd,:sexe,:mail)');
+            $query->execute([':pseudo'=>$pseudo,':pwd'=>$pwd,':sexe'=>$sexe,':mail'=>$adressemail]);
+        }
+
+
+
+
+
+
+
+    }
