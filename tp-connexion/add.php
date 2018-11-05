97545b0f32578dab05a014939039d1c42dbaf97b
elvis kankola <elviskankola1@gmail>
2018-11-05 09:32:18 +0200

    UPDATE

--- tp-connexion/add.php
+++ tp-connexion/add.php
@@ -1,18 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>AJOUT</title>
-</head>
-<html>
-    <form>
-        <input type='text' placeholder="entrez votre pseudo" name="pseudo"><br><br>
-        <input type="email" placeholder="entrez votre adresse e-mail" name="mail"><br><br>
-        <input type="password" placeholder="entrez votre mot de passe" name="pwd"><br><br>
-        <input type="password" placeholder="confirmez votre mot de passe" name="pwdc"><br><br>
-       <select name="sexe">
-           <option>M</option>
-           <option>F</option>
-       </select><br><br>
-       <input type="submit" value="ADD">
-    </form>
-</html>
+<?php
+    echo 'hello';
