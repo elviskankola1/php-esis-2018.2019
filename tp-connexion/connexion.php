534fd7e687bd556343d53532be1b129cbae4900f
elvis kankola <elviskankola1@gmail>
2018-11-05 10:24:37 +0200

    UPDATE

--- tp-connexion/connexion.php
+++ tp-connexion/connexion.php
@@ -3,11 +3,12 @@
      if ($_SERVER['REQUEST_METHOD'] =='POST') {
         $pseudo = strip_tags(trim($_POST['pseudo']));
         $pwd = sha1(strip_tags(trim($_POST['pwd'])));
-        $user = $db->prepare('SELECT (pseudo,pwd) INTO user WHERE (pseudo =:pseudo AND pwd =: pwd)');
-        if ($user) {
+        $user = $db->prepare('SELECT pseudo,pwd INTO user WHERE pseudo =$pseudo AND pwd =$pwd');
+        /*if ($user) {
             echo "COMPTE VALIDE";
         }else{
             $_SERVER['HTTP_REFERER'];
-        }
+        }*/
+        echo $user->pseudo;
 
      }
