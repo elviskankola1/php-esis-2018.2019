9d3916980530274af0926522afc42c38308fc527
elvis kankola <elviskankola1@gmail>
2018-11-05 10:14:11 +0200

    UPDATE

--- tp-connexion/add.php
+++ tp-connexion/add.php
@@ -2,8 +2,8 @@
     $db = new PDO('mysql:host=localhost;dbname=data-try','root','');
     if ($_SERVER['REQUEST_METHOD']=='POST') {
         $pseudo = strip_tags(trim($_POST['pseudo']));
-        $pwd = strip_tags(trim($_POST['pwd']));
-        $pwdc = strip_tags(trim($_POST['pwdc']));
+        $pwd = sha1(strip_tags(trim($_POST['pwd'])));
+        $pwdc = sha1(strip_tags(trim($_POST['pwdc'])));
         $sexe = $_POST['sexe'];
         $adressemail = strip_tags(trim($_POST['mail']));
         if (empty($pseudo) OR empty($pwd) OR empty($pwdc) OR empty($sexe) OR empty($adressemail) OR !isset($pseudo,$pwd,$pwdc,$sexe,$adressemail)) {
@@ -13,6 +13,7 @@
             if ($pwd === $pwdc) {
                 $query = $db->prepare('INSERT INTO user(pseudo,pwd,sexe,mail) VALUES (:pseudo,:pwd,:sexe,:mail)');
                 $query->execute([':pseudo'=>$pseudo,':pwd'=>$pwd,':sexe'=>$sexe,':mail'=>$adressemail]);
+                header('location:uiconnexion.php');
             }else {
                 header('location:uiadd.php');
             }
