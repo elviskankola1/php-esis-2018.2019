135fe754ae193ece6ba50f1d35d6f3faacbe3961
elvis kankola <elviskankola1@gmail>
2018-11-05 10:15:59 +0200

    UPDATE

--- tp-connexion/connexion.php
+++ tp-connexion/connexion.php
@@ -1,8 +1,8 @@
 <?php
      $db = new PDO('mysql:host=localhost;dbname=data-try','root','');
      if ($_SERVER['REQUEST_METHOD'] =='POST') {
-        $pseudo = $_POST['pseudo'];
-        $pwd = $_POST['pwd'];
+        $pseudo = strip_tags(trim($_POST['pseudo']));
+        $pwd = sha1(strip_tags(trim($_POST['pwd'])));
         $user = $db->prepare('SELECT (pseudo,pwd) INTO user WHERE (pseudo =:pseudo AND pwd =: pwd)');
         if ($user) {
             echo "COMPTE VALIDE";
